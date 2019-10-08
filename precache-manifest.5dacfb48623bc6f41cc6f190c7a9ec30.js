e485f6881fbdb56719aeffee61928df8a1b2f381
edn9 <eduardohenriquen9@outlook.com>
2019-10-07 23:10:47 -0300

    update

---
+++ precache-manifest.5dacfb48623bc6f41cc6f190c7a9ec30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aefe34924548bf5eb051f37338f3a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5058ef0e244d0641694",
+    "url": "/static/css/main.d6fec8ef.chunk.css"
+  },
+  {
+    "revision": "ff99af9f36ab3acaf605",
+    "url": "/static/js/2.d63f4fab.chunk.js"
+  },
+  {
+    "revision": "d5058ef0e244d0641694",
+    "url": "/static/js/main.48b58691.chunk.js"
+  },
+  {
+    "revision": "eb98a4f675d2e4824cff",
+    "url": "/static/js/runtime-main.a6aef668.js"
+  },
+  {
+    "revision": "10cd602ec9be8d08f692572ce9621294",
+    "url": "/static/media/plant.10cd602e.jpg"
+  }
+]);
